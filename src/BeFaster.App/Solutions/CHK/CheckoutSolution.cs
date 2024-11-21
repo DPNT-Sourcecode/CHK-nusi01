e9b901757966017adee12088261c4ffa72e13c14
sbx_user1051 <sbx_user1051@169.254.107.205>
2024-11-21 16:06:39 +0000

    Thu Nov 21 16:06:39 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -7,31 +7,36 @@ namespace BeFaster.App.Solutions.CHK
         public static int ComputePrice(string? skus)
         {
             int count = 0;
-            if (skus != null && skus.Length > 0)
+            if (skus != null)
             {
-                if (skus.Any(x => x != 'A' && x != 'B' && x != 'C' && x != 'D'))
+                if (skus.Length > 0)
                 {
-                    return -1;
-                }
-                else
-                {
-                    int countA = skus.Count(x => x == 'A');
-                    int countB = skus.Count(x => x == 'B');
-                    int countC = skus.Count(x => x == 'C');
-                    int countD = skus.Count(x => x == 'D');
-                    count += countC * 20;
-                    count += countD * 15;
-                    int wholeA = countA / 3;
-                    int decimalA = countA % 3;
-                    int wholeB = countB / 2;
-                    int decimalB = countB % 2;
-                    count += wholeA * 130;
-                    count += decimalA * 50;
-                    count += wholeB * 45;
-                    count += decimalB * 30;
-                    return count;
 
+                    if (skus.Any(x => x != 'A' && x != 'B' && x != 'C' && x != 'D'))
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        int countA = skus.Count(x => x == 'A');
+                        int countB = skus.Count(x => x == 'B');
+                        int countC = skus.Count(x => x == 'C');
+                        int countD = skus.Count(x => x == 'D');
+                        count += countC * 20;
+                        count += countD * 15;
+                        int wholeA = countA / 3;
+                        int decimalA = countA % 3;
+                        int wholeB = countB / 2;
+                        int decimalB = countB % 2;
+                        count += wholeA * 130;
+                        count += decimalA * 50;
+                        count += wholeB * 45;
+                        count += decimalB * 30;
+                        return count;
+
+                    }
                 }
+                else { return 0; }
 
             }
             else
@@ -43,4 +48,5 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
+
 
