cb29d846f4f3f8ea9d0531dfd0a1ec802fc1732c
sbx_user1051 <sbx_user1051@169.254.107.205>
2024-11-21 15:55:16 +0000

    Thu Nov 21 15:55:16 UTC 2024

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -6,13 +6,23 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int ComputePrice(string? skus)
         {
+            int count = 0;
             if (skus != null && skus.Length > 0)
             {
-                char[] charArray = new char[skus.Length];
-                int countA = skus.Count(x => x == 'A');
-                int countB = skus.Count(x => x == 'A');
-                int countC = skus.Count(x => x == 'A');
-                int countD = skus.Count(x => x == 'A');
+                if (skus.Any(x => x != 'A' || x != 'B' || x != 'C' || x != 'D'))
+                {
+                    return -1;
+                }
+                else
+                {
+                    char[] charArray = new char[skus.Length];
+                    int countA = skus.Count(x => x == 'A');
+                    int countB = skus.Count(x => x == 'B');
+                    int countC = skus.Count(x => x == 'C');
+                    int countD = skus.Count(x => x == 'D');
+                    
+
+                }
 
             }
             else
@@ -24,6 +34,7 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
+
 
 
 
